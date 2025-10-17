4dcb52ff95fd89cfd7fbce2133595d275d635661
formica2 <soz111@bk.ru>
2025-10-18 02:40:07 +0300

    init mod

---
+++ SaveInventoryMod/ModBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace SaveInventoryMod
+{
+
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        private const string Id = "Formica.SaveInventoryMod";
+        private Harmony harmony;
+        void Awake()
+        {
+            Debug.Log("SaveInventoryMod Loaded!!!");
+        }
+        void OnDestroy()
+        {
+        }
+        void OnEnable()
+        {
+            harmony = new Harmony(Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Debug.Log("SaveInventoryMod ENABLED!!!");
+
+        }
+        void OnDisable()
+        {
+            harmony.UnpatchAll(Id);
+            Debug.Log("SaveInventoryMod DISABLED!!!");
+        }
+    }
+}
---
+++ SaveInventoryMod/Patches.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using ItemStatsSystem;
+using System;
+using UnityEngine;
+
+namespace SaveInventoryMod
+{
+    [HarmonyPatch(typeof(CharacterMainControl))]
+    [HarmonyPatch("DropAllItems")]
+    class CharacterMainControl_DropAllItems
+    {
+        static bool Prefix()
+        {
+            Debug.Log("DisplayItemValue DropAllItems");
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CharacterMainControl))]
+    [HarmonyPatch("DestroyAllItem")]
+    class CharacterMainControl_DestroyAllItem
+    {
+        static bool Prefix()
+        {
+            Debug.Log("DisplayItemValue DestroyAllItem");
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(LevelConfig), "SpawnTomb", MethodType.Getter)]
+    class LevelConfig_SpawnTomb
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
